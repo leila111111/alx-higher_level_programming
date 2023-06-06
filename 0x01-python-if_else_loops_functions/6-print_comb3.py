992f276ecef243db9c88e1ae8527878ac2177f8b
leila111111 <leila_merzak@outlook.fr>
2023-06-06 13:14:39 -0700

    my commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(i + 1,10):
+    for j in range(i + 1, 10):
         print("{:02d}, ".format(i * 10 + j), end="")
 print("{:02d}".format(98))
