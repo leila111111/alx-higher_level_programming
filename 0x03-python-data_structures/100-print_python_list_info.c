490388552c899a53e9315fbb938b2ffb1f22fae1
leila111111 <leila_merzak@outlook.fr>
2023-06-12 15:05:25 -0700

    my commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+void print_python_list_info(PyObject *p)
+{
+	int size = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		printf("%s\n", Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
+}
