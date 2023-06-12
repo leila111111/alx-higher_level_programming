319960e38b8f0f5b5d067c2fcea0a2e86529a8af
leila111111 <leila_merzak@outlook.fr>
2023-06-12 14:30:35 -0700

    my commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,8 +5,8 @@ def max_integer(my_list=[]):
         else:
             maximum = my_list[0]
             for i in range(len(my_list)):
-                if my_list[i] > maximum:
+                if (my_list[i] > maximum):
                     maximum = my_list[i]
                 else:
                     maximum = maximum
-            return maximum       
+            return (maximum)
