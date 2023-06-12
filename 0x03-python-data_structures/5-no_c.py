94cccdc6fc503ce3eacc0f835f26555e5a287518
leila111111 <leila_merzak@outlook.fr>
2023-06-12 13:44:25 -0700

    my commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string_deleted = ""
     for i in my_string:
-        if  i != 'C' and i != 'c':
+        if (i != 'C') and (i != 'c'):
             string_deleted += i
     return (string_deleted)
