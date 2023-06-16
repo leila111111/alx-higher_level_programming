101c78137b215f06d21daa678c6b771bf864ecc5
leila111111 <leila_merzak@outlook.fr>
2023-06-16 16:58:46 -0700

    my commit

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = []
+    res = 0
+    for i in my_list:
+        if i not in new_list:
+            res += i
+            unique.append(i)
+    retur(res)
