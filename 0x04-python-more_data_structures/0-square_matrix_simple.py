6952cc23e5ffd1fc79ce56c1bd20d2572624b514
leila111111 <leila_merzak@outlook.fr>
2023-06-16 16:43:22 -0700

    my commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,8 @@ def square_matrix_simple(matrix=[]):
     square = []
 
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            square[i][j] = matrix[i][j] ** 2
-
+        res = []
+        for j in range(len(matrix[0])):
+            res.append(matrix[i][j] ** 2)
+        square.append(res)
     return (square)
