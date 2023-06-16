ba932490733186047244b2d68a61c56bad06c597
leila111111 <leila_merzak@outlook.fr>
2023-06-16 16:53:32 -0700

    my commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
 
     for i in my_list:
         if i == search:
-             res.append(replace)
+            res.append(replace)
         else:
             res.append(i)
     return (res)
