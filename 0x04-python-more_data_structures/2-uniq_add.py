1ce9cb169518e8c5c6d75715bfbcc2600432f6f1
leila111111 <leila_merzak@outlook.fr>
2023-06-16 17:00:21 -0700

    my commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     unique = []
     res = 0
     for i in my_list:
-        if i not in new_list:
+        if i not in unique:
             res += i
             unique.append(i)
     retur(res)
