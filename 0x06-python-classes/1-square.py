9364ed4e8b5f4f3bb84803b55b1be54d589782ae
leila111111 <leila_merzak@outlook.fr>
2023-06-27 10:14:57 -0700

    my commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""empty class Square that defines a square:"""
+
+
+class Square:
+    """ class Square with private attribute size:"""
+    def __init__(self, size = 0):
+    """initialisation"""
+        self.__size__ = size       
