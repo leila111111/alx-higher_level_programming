de929e8b2a353c26bcb3059526f6ab2bbc98e238
leila111111 <leila_merzak@outlook.fr>
2023-06-26 17:40:41 -0700

    my commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     j = 0
-    for i in my_list:
+    for i in range(x):
         try:
-            print(i, end=' ')
+            print(my_list[i], end=' ')
             j += 1
         except IndexError:
             break
