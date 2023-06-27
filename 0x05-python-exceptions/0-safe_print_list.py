9167a1c13d32fc3b5925d217c8ea74a23e1e39de
leila111111 <leila_merzak@outlook.fr>
2023-06-26 17:51:55 -0700

    my commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end=' ')
             j += 1
-        except IndexError:
+        except Exception as error:
             break
-    print("")
+    print()
     return (j)
