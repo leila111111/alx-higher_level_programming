8b9e63bdc09bda022b9565cac54aa9af2ad04728
leila111111 <leila_merzak@outlook.fr>
2023-06-26 18:10:19 -0700

    my commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     for j in range(x):
-        try : 
-             print("{:d}".format(my_list[j]), end='')
-             j += 1
-        except Exception as error :
+        try:
+            print("{:d}".format(my_list[j]), end='')
+            i += 1
+        except Exception as error:
             pass
     print()
-    return (j)
+    return (i)
