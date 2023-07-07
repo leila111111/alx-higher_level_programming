cc6584ab62ae7e5d27ae9eb68fd31376025f7542
leila111111 <leila_merzak@outlook.fr>
2023-07-07 14:48:20 +0000

    my commit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,25 +5,30 @@ unittest to test max_integer
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-        """
-        class to test the max_integer function
-        """
-        def test_one(self):
+    """
+    class to test the max_integer function
+    """
+    def test_one(self):
         """ test one """
-            self.assertEqual(max_integer(), None)
-            self.assertEqual(max_integer([]), None)
-            self.assertEqual(max_integer(""), None)
-        def test_two(self):
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(""), None)
+
+    def test_two(self):
         """test 2 """
-            self.assertEqual(max_integer([1]), 1)
-            self.assertEqual(max_integer([1.2, 2, 3]), 3)
-            self.assertEqual(max_integer([-1, -2, -3]), -1)
-            self.assertEqual(max_integer([1, 2, 3]), 3)
-            self.assertEqual(max_integer([-1]), -1)
-        def test_three(self):
-            self.assertEqual(max_integer("milo"), "o")
-            self.assertEqual(max_integer(["donga", "zebra", "zebra")
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1.2, 2, 3]), 3)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([-1]), -1)
+
+    def test_three(self):
+        """test 3"""
+        self.assertEqual(max_integer("milo"), "o")
+        self.assertEqual(max_integer(["donga", "zebra"]), "zebra")
+
 
 if __name__ == '__main__':
     unittest.main()
