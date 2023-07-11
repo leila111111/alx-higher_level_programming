c440ad0491d99481ad4db97787a05699e6faa6ee
leila111111 <leila_merzak@outlook.fr>
2023-07-11 21:47:47 +0000

    my commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that reads a text
+file (UTF8) and prints it to 
+stdout """
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+    with open(filename, encoding="utf-8") as fr:
+        print(fr.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that writes a string
+to a text file (UTF8) and returns
+the number of characters written:"""
+
+
+
+def write_file(filename="", text=""):
+    """function that write into
+    a file"""
+    with open(filename, "w") as fr:
+        return fr.write(text)
