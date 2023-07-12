daa10e661e5267f93feadc2c19ac4df7a0d92295
leila111111 <leila_merzak@outlook.fr>
2023-07-12 00:28:57 +0000

    my commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
     try:
-        loads = load_from_json_file("add_item.json")
+        name = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        loads = []
-    loads.extend(sys.argv[1:])
-    save_to_json_file(loads, "add_item.json")
+        name = []
+    name.extend(sys.argv[1:])
+    save_to_json_file(name, "add_item.json")
