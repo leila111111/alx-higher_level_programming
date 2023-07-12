830c9a5ca820b6f18a12abaa21834885949eed73
leila111111 <leila_merzak@outlook.fr>
2023-07-12 00:41:47 +0000

    my commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,13 +2,16 @@
 """class Student that defines a student
 """
 
+
 class Student:
+
     """defining student"""
     def __init__(self, first_name, last_name, age):
         """a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """retrieves a dictionary representation of
         a Student instance """
