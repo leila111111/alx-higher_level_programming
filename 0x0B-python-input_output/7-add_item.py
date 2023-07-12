74844508d12c21ac83eebfc7c3493d3def25942c
leila111111 <leila_merzak@outlook.fr>
2023-07-12 00:32:05 +0000

    my commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+
     try:
         name = load_from_json_file("add_item.json")
     except FileNotFoundError:
         name = []
-    name.extend(sys.argv[1:])
+    for i in sys.argv[1:]:
+        name.append(i)
     save_to_json_file(name, "add_item.json")
