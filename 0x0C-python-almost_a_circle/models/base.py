29bc6a613c4cfde66dbd2196445c1b5c70523cae
leila111111 <leila_merzak@outlook.fr>
2023-07-17 16:32:06 -0700

    my commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,17 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """"class method that writes the JSON string representation
+        of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                dictio_list = []
+                for i in list_objs:
+                    dictio_list.append(i.to_dictionary())
+                jsonfile.write(Base.to_json_string(dictio_list))
