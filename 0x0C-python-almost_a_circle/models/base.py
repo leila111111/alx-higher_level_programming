d363258b447889d5e4f63543bf749554777e02d5
leila111111 <leila_merzak@outlook.fr>
2023-07-17 19:53:51 +0000

    my commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,9 +4,11 @@ import json
 import csv
 import turtle
 
+
 class Base:
     """class base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """class  constructor"""
         if id is not None:
