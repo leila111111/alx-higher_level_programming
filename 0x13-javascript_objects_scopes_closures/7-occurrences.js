32f4c060d46abb43b47ef50d6f1fc37d45e29b34
leila111111 <leila_merzak@outlook.fr>
2023-09-12 19:36:30 +0000

    first commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let n = 0;
   for (const i = 0; i < list.length; i++) {
-    if (searchElement == i) {
+    if (searchElement === i) {
       n++;
     }
   }
