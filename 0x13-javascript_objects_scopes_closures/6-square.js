df4dec9ec3137e482c101050b39703f11af16ff9
leila111111 <leila_merzak@outlook.fr>
2023-09-12 19:23:13 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const FirstSquare = require('./5-square');
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
