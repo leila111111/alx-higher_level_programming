b6243bea0734a45618a550a937edc370ae7c86c0
leila111111 <leila_merzak@outlook.fr>
2023-09-17 23:41:36 +0000

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  script that lists all cities from the database hbtn_0e_4_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    curs = cur.fetchall()
+    print(", ".join(i[0] for i in curs))
+    cur.close()
+    db.close()
