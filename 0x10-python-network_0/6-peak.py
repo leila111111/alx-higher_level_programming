23b2e5cfdaa7e8be2ff79da02b4fd97d47cc926f
leila111111 <leila_merzak@outlook.fr>
2023-09-29 00:49:12 +0000

    tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,13 +6,14 @@ def find_peak(list_of_integers):
     """function that finds a peak
     in a list of unsorted integers."""
 
-    if len(list_of_integers) == 0:
+    lenght = len(list_of_integers)
+    if lenght == 0:
         return None
-    if len(list_of_integers) == 2:
+    if lenght == 2:
         return max(list_of_integers)
-    if len(list_of_integers) == 1:
+    if lenght == 1:
         return list_of_integers[0]
-    i = int(len(list_of_integers) / 2)
+    i = int(lenght / 2)
     if i > 0 and list_of_integers[i] < list_of_integers[i + 1]:
         return find_peak(list_of_integers[i:])
     if list_of_integers[i] >= list_of_integers[i - 1] and\
