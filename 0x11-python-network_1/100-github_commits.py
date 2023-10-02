baabc908e16cbfd888fbae5f8c9d1e06ae69f7fb
leila111111 <leila_merzak@outlook.fr>
2023-10-02 19:13:59 +0000

    task10--100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,3 +2,13 @@
 """Python script that takes 2 arguments
 in order to list the 10 most recent commits on a
 GitHub repository"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+    for i in range(10):
+        print(commits[i].get("sha"), end=': ')
+        print(commits[i].get("commit").get("author").get("name"))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,9 +3,8 @@
 sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response."""
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(req) as resp:
-        print((resp.headers).get("X-Request-Id"))
+    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(req.text)
