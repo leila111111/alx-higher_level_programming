f57ef00144981410996faf99e5b4382fc681cf42
leila111111 <leila_merzak@outlook.fr>
2023-10-02 18:52:19 +0000

    task4--10

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 """Python script that takes in a letter and sends
 a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import sys
+import requests
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        flop = ""
+    else:
+        flop = sys.argv[1]
+    value = {"q": flop}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=value)
+    try:
+        resp = req.json()
+        if resp == {}:
+            print("No result")
+        else:
+            print(f"[{resp['id']}] {resp['name']}")
+    except ValueError:
+        print("Not a valid JSON")
