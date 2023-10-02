aa88cba5758e8eb9b0da7a0d77be1eddeb4c09c6
leila111111 <leila_merzak@outlook.fr>
2023-10-02 19:20:38 +0000

    task10--100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     req = requests.get(url)
     commits = req.json()
-    for i in range(10):
-        print(commits[i].get("sha"), end=': ')
-        print(commits[i].get("commit").get("author").get("name"))
+    try:
+        for i in range(10):
+            print(commits[i].get("sha"), end=': ')
+            print(commits[i].get("commit").get("author").get("name"))
+    except IndexError:
+        pass
