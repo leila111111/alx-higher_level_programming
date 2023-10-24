1f112b13a1b7e3c8d499c20b4a874172507b022b
leila111111 <leila_merzak@outlook.fr>
2023-10-24 22:47:31 +0000

    tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  if (response.statusCode === 200) {
+    const result = {};
+    const responses = JSON.parse(body);
+    for (const i in responses) {
+      const ji = responses[i];
+      if (ji.result) {
+        if (result[ji.userId] === undefined) {
+          result[ji.userId] = 1;
+        } else {
+          result[ji.userId]++;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
